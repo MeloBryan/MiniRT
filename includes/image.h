8eaa98a6458b2f4bd474fba0d0d93fa2a73cee3f
BMelo <bryanmelo64@outlook.com>
2026-06-10 05:30:26 +0200

    add: calcul for a better render on the srceen

--- includes/image.h
+++ includes/image.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   image.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmelo <bmelo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 17:26:02 by bmelo             #+#    #+#             */
-/*   Updated: 2026/06/09 17:29:50 by bmelo            ###   ########.fr       */
+/*   Updated: 2026/06/10 05:09:51 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,18 @@
 
 typedef struct s_data
 {
-	void	*mlx;
-	void	*win;
-	double	x;
-	double	y;
-	double	z;
+	void		*mlx;
+	void		*win;
+	t_vector	cam_pos;
 
-	int		max_iterations;
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-	int		key[65536];
-	double	zoom;
+	int			max_iterations;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+	int			key[65536];
+	double		zoom;
 }				t_data;
 
 void	close_win(t_data *data);
--- src/Image/renderer.c
+++ src/Image/renderer.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   render_scene.c                                     :+:      :+:    :+:   */
+/*   renderer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmelo <bmelo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 16:41:00 by bmelo             #+#    #+#             */
-/*   Updated: 2026/06/09 17:48:08 by bmelo            ###   ########.fr       */
+/*   Updated: 2026/06/10 05:18:41 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,14 @@ void	render_scene(t_data *data)
 {
 	int		x;
 	int		y;
+	double	uv_x;
+	double	uv_y;
+	double	screen_x;
+	double	screen_y;
+	double	aspect_ratio;
+	t_ray	ray;
 
+	aspect_ratio = (double)WIDTH / (double)HEIGHT;
 	y = 0;
 	while (y < HEIGHT)
 	{
@@ -24,7 +31,23 @@ void	render_scene(t_data *data)
 		printf("Scanlines remaining : %d\n", (HEIGHT - y));
 		while (x < WIDTH)
 		{
-			my_mlx_pixel_put(data, x, y, (x * 255 / WIDTH) << 16);
+			uv_x = (double)x / WIDTH;
+			uv_y = (double)y / HEIGHT;
+			screen_x = (2.0 * uv_x - 1.0) * aspect_ratio;
+			screen_y = 1.0 + 2.0 * uv_y;
+			ray.origin.x = 0.0;
+			ray.origin.y = 0.0;
+			ray.origin.z = 0.0;
+			ray.direction.x = screen_x;
+			ray.direction.y = screen_y;
+			ray.direction.z = -1.0;
+			ray.direction = normalization(ray.direction);
+			//fonction d'appel
+			int r = (int)((ray.direction.x + 1.0) * 127.5);
+			int g = (int)((ray.direction.y + 1.0) * 127.5);
+			int b = (int)((ray.direction.z + 1.0) * 127.5);
+			int color = (r << 16) | (g << 8) | b;
+			my_mlx_pixel_put(data, x, y, color);
 			x++;
 		}
 		y++;
