b806003dba931e3311a9f615f2ee7611bd36e190
Erwann Defoy <ecdefoy1@gmail.com>
2026-07-08 14:11:48 +0200

    feat(parsing): propagate parse errors up to parse_file

--- src/Parsing/parse_camera.c
+++ src/Parsing/parse_camera.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   parse_camera.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: edefoy <edefoy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/13 01:41:41 by marvin            #+#    #+#             */
-/*   Updated: 2026/06/13 01:41:41 by marvin           ###   ########.fr       */
+/*   Created: 2026/07/08 14:08:52 by edefoy            #+#    #+#             */
+/*   Updated: 2026/07/08 14:08:52 by edefoy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-void    parse_camera(char *line, t_data *data)
+int parse_camera(char *line, t_data *data)
 {
     char    **tokens;
     char    **pos_tokens;
@@ -21,24 +21,24 @@ void    parse_camera(char *line, t_data *data)
     if (data->camera_already_set == 1)
     {
         printf("Error\nMultiple camera detected\n");
-        return ;
+        return (0);
     }
     tokens = ft_split_spaces(line);
     printf("DEBUG: matrix_length = %d\n", matrix_length(tokens));
     for (int k = 0; tokens && tokens[k]; k++)
         printf("  tokens[%d] = '%s'\n", k, tokens[k]);
     if (!tokens)
-        return ;
+        return (0);
     if (matrix_length(tokens) != 4)
     {
         printf("Error\nInvalid camera format\n");
-        return (free_matrix(tokens));
+        return (free_matrix(tokens), 0);
     }
     pos_tokens = ft_split(tokens[1], ',');
     if (matrix_length(pos_tokens) != 3)
     {
         printf("Error\nPOSITION must have 3 values (x,y,z)\n");
-        return (free_matrix(tokens), free_matrix(pos_tokens));
+        return (free_matrix(tokens), free_matrix(pos_tokens), 0);
     }
     data->cam_pos.x = ft_atof(pos_tokens[0]);
     data->cam_pos.y = ft_atof(pos_tokens[1]);
@@ -47,7 +47,7 @@ void    parse_camera(char *line, t_data *data)
     if (matrix_length(dir_tokens) != 3)
     {
         printf("Error\nDIRECTION must have 3 values (x,y,z)\n");
-        return (free_matrix(tokens), free_matrix(pos_tokens));
+        return (free_matrix(tokens), free_matrix(pos_tokens), 0);
     }
     data->cam_dir.x = ft_atof(dir_tokens[0]);
     data->cam_dir.y = ft_atof(dir_tokens[1]);
@@ -57,16 +57,17 @@ void    parse_camera(char *line, t_data *data)
         data->cam_dir.z < -1.0 || data->cam_dir.z > 1.0)
     {
         printf("Error\nInvalid direction value(must be between -1 and 1)\n");
-        return (free_matrix(tokens), free_matrix(pos_tokens), free_matrix(dir_tokens));
+        return (free_matrix(tokens), free_matrix(pos_tokens), free_matrix(dir_tokens), 0);
     }
     data->cam_fov = ft_atof(tokens[3]);
     if (data->cam_fov < 0 || data->cam_fov > 180)
     {
         printf("Error\nFOV velue must be between 0 and 180\n");
-        return (free_matrix(tokens), free_matrix(pos_tokens), free_matrix(dir_tokens));
+        return (free_matrix(tokens), free_matrix(pos_tokens), free_matrix(dir_tokens), 0);
     }
     data->camera_already_set = 1;
     free_matrix(tokens);
     free_matrix(pos_tokens);
     free_matrix(dir_tokens);
+    return (1);
 }
--- src/Parsing/parse_line.c
+++ src/Parsing/parse_line.c
@@ -28,44 +28,43 @@ static void	strip_line_end(char *line)
 int parse_file(char *file_name, t_data *data)
 {
     int     fd;
+	int		ok;
     char    *line;
 
     fd = open(file_name, O_RDONLY);
     if (fd < 0)
         return (printf("Error\nCould not open file\n"), 0);
-    while ((line = get_next_line(fd)) != NULL)
+    ok = 1;
+	while ((line = get_next_line(fd)) != NULL)
     {
-        if (line && line[0] != '\n')
-            parse_line(line, data);
+		strip_line_end(line);
+        if (ok)
+            ok = parse_line(line, data);
         free(line);
     }
     close(fd);
-    return (1);
+    return (ok);
 }
 
-void    parse_line(char *line , t_data *data)
+int	parse_line(char *line , t_data *data)
 {
     while (*line == ' ' || *line == '\t')
         line++;
     if (*line == '\0' || *line == '\n' || *line == '#')
-        return ;
+        return (1);
     else if (line[0] == 'A' && (line[1] == ' ' || line[1] == '\t'))
-        parse_ambient(line, data);
+        return (parse_ambient(line, data));
     else if (line[0] == 'C' && (line[1] == ' ' || line[1] == '\t'))
-         parse_camera(line, data);
+        return (parse_camera(line, data));
     else if (line[0] == 'L' && (line[1] == ' ' || line[1] == '\t'))
-        parse_light(line, data);
+        return (parse_light(line, data));
     // else if (line[0] == 's' && line[1] == 'p' && (line[2] == ' ' || line[2] == '\t'))
     //     parse_sphere(line + 2, data);
     // else if (line[0] == 'p' && line[1] == 'l' && (line[2] == ' ' || line[2] == '\t'))
     //     parse_plane(line + 2, data);
     // else if (line[0] == 'c' && line[1] == 'y' && (line[2] == ' ' || line[2] == '\t'))
     //     parse_cylinder(line + 2, data);
-    else
-    {
-        printf("Error\nUnknown element identifier : %c\n", line[0]);
-        //set flag pour erreur
-    }
+    return (printf("Error\nUnknown element identifier : %c\n", line[0]), 0);
 }
 
 int matrix_length(char **tokens)
